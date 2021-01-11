d3d8933dd67bdfd0bfb067f33ad8e0c2bcee13ed
ferror18 <ferror18@gmail.com>
2021-01-11 13:13:47 -0800

    Database connection

--- vbb_backend/users/management/commands/faker.py
+++ vbb_backend/users/management/commands/faker.py
@@ -1,5 +1,6 @@
 from faker import Faker
 from django.core.management.base import BaseCommand, CommandError
+import psycopg2
 # from polls.models import Question as Poll
 
 class Command(BaseCommand):
@@ -9,8 +10,16 @@ class Command(BaseCommand):
     #     parser.add_argument('poll_ids', nargs='+', type=int)
 
     def handle(self, *args, **options):
+        conn = psycopg2.connect("dbname=vbb user=postgres")
+        cur = conn.cursor()
         fake = Faker()
-        print(fake.name())
+        cur.execute("SELECT * FROM users_user;")
+        print(cur.fetchone())
+#         cur.execute("""
+# ...     INSERT INTO some_table (an_int, a_date, a_string)
+# ...     VALUES (%s, %s, %s);
+# ...     """,
+# ...     (10, datetime.date(2005, 11, 18), "O'Reilly"))
         # for poll_id in options['poll_ids']:
         #     try:
         #         poll = Poll.objects.get(pk=poll_id)
@@ -21,3 +30,5 @@ class Command(BaseCommand):
         #     poll.save()
 
         #     self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+        cur.close()
+        conn.close()
