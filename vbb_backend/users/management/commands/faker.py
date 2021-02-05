e73b402ddd72a418a9962736d46d277ede142b67
ferror18 <ferror18@gmail.com>
2021-02-04 23:35:02 -0800

    Big commit reamke of a lot of th eflow

--- vbb_backend/users/management/commands/faker.py
+++ vbb_backend/users/management/commands/faker.py
@@ -1,5 +1,6 @@
 from faker import Faker
 from django.core.management.base import BaseCommand, CommandError
+from django.db import IntegrityError
 from pprint import pprint
 import datetime
 from uuid import uuid4
@@ -11,27 +12,41 @@ fake = Faker()
 today = pytz.utc.localize(datetime.datetime.utcnow())
 import psycopg2
 
+def calcPerc(total, current):
+    return 0 if current == 0 else (current*100)//total
+
+def verbose(*args, end='\n'):
+    if (True):
+        for i in args:
+            print(i, end=end)
+# Script Variables
+
+NUM_OF_HEADMASTERS_AND_PROGRAMS_AND_SCHOOLS = 1000
+NUM_OF_MENTORS = NUM_OF_HEADMASTERS_AND_PROGRAMS_AND_SCHOOLS * 12
+NUM_OF_CLASSROOMS = NUM_OF_HEADMASTERS_AND_PROGRAMS_AND_SCHOOLS * 3
+NUM_OF_STUDENTS = NUM_OF_CLASSROOMS * 25
 class Command(BaseCommand):
     #This function generates and inserts Users into db
-    def genUser(self, user_type):
-        fakeProfile = fake.simple_profile()
+    def genUser(self, user_type, amount=1):
+        fn = fake.first_name()
+        ln = fake.last_name()
         new_user = User.objects.create(
             password='password',
             last_login=today,
             is_superuser=False,
-            username=fakeProfile['username'],
-            first_name=fakeProfile['name'].split()[0],
-            last_name=fakeProfile['name'].split()[1],
-            email=fake.email(),
+            username= fn + ln + str(fake.random_int()),
+            first_name= fn,
+            last_name= ln,
+            email= fake.email(),
             is_staff=True,
             is_active=True,
             date_joined=today,
             user_type=user_type,
             verification_level='1',
-            vbb_chapter='vbbchapter',
+            vbb_chapter='vbbchapter' + fake.word(),
             primary_language=fake.language_name(),
             time_zone=fake.timezone(),
-            initials=fakeProfile['name'].split()[0][0] + fakeProfile['name'].split()[1][0],
+            initials=fn[0] + ln[0],
             personal_email=fake.free_email(),
             phone=fake.phone_number(),
             occupation=fake.paragraph(nb_sentences=1),
@@ -44,11 +59,11 @@ class Command(BaseCommand):
             modified_date=today,
             date_of_birth=today
         )
+        return new_user
 
-
-        return 
     #This function generates and inserts Students into db
-    def genStudent(self, userId, classroom_id, school_level):
+    def genStudent(self, data):
+        userId, classroom_id = data
         Student.objects.create(
             created_date=today,
             modified_date=today,
@@ -56,11 +71,11 @@ class Command(BaseCommand):
             external_id=uuid4(),
             user_id=userId,
             classroom_id=classroom_id,
-            school_level=school_level
+            school_level=fake.random_int(0, 100)
         )
     #This function generates and inserts the program into db
     def genProgram(self, program_director_id):
-        Program.objects.create(
+        new_prog = Program.objects.create(
             created_date=today,
             modified_date=today,
             deleted=False,
@@ -75,10 +90,11 @@ class Command(BaseCommand):
             default_language=fake.language_name(),
             program_director_id=program_director_id
         )
+        return f'ProgId: {new_prog.id}, Director: {new_prog.program_director_id}'
     #This function generates and inserts school into db
     def genSchool(self, program_id):
         lat,lon=fake.latlng()
-        School.objects.create(
+        new_school = School.objects.create(
             created_date=today,
             modified_date=today,
             deleted=False,
@@ -88,9 +104,10 @@ class Command(BaseCommand):
             latitude=lat,
             program_id=program_id
         )
+        return f'SchoolId: {new_school.id}, Program: {new_school.program_id}'
     #This function generates and inserts classroom into db
     def genClassroom(self, school_id):
-        Classroom.objects.create(
+        new_classroom = Classroom.objects.create(
             created_date=today,
             modified_date=today,
             deleted=False,
@@ -98,6 +115,7 @@ class Command(BaseCommand):
             name=fake.company(),
             school_id=school_id
         )
+        return f'ClassroomId: {new_classroom.id}, SchoolId: {new_classroom.school_id}'
     #This function generates and inserts Mentoors into db
     def genMentor(self, user_id):
         Mentor.objects.create(
@@ -112,10 +130,49 @@ class Command(BaseCommand):
             desired_involvement=None,
             is_adult=None
         )
+    
+    def exceptionHandlingLoop(self, callback, amount, paramForCallback=None):
+            toGenerate, IntE, DataE, UniqE, operations,totalErrors = 0,0,0,0,0,0
+            
+            while toGenerate < amount:
+                try:
+                    operations+=1
+                    if (type(paramForCallback) is list):
+                        result = callback(paramForCallback.pop(fake.random_int(0, len(paramForCallback)-1)))
+                        # print(result)
+                    elif (paramForCallback):
+                        result = callback(paramForCallback)
+                    else:
+                        callback()
+                except IntegrityError as exc:
+                    IntE+=1 #Counter for Integrity errors
+                    totalErrors+=1 #Counter for all errors
 
+                except Exception as exc:
+                    ExcName = exc.__class__.__name__
+                    totalErrors+=1 #Counter for all errors
+                    if (ExcName == 'UniquenessException'):
+                        UniqE+=1 #Counter for Uniqueness excep error
+                    elif (ExcName == 'DataError'):
+                        DataE+=1 #Counter for DataErrors
+                    else:
+                        raise exc
+                else:
+                    toGenerate+=1
+                    Exec = f"Executing {callback.__name__} #{operations:,} | {calcPerc(amount,toGenerate)}% | "
+                    IntegrityString = f"IntegrityError: {IntE:,} {calcPerc(operations, IntE)}%  "
+                    UniqueString = f"UniquenessException: {UniqE:,} {calcPerc(operations, UniqE)}%  "
+                    dataErrString = f"DataError: {DataE:,} {calcPerc(operations, DataE)}%  "
+                    errRateString = f"Total Errors: {totalErrors:,} {calcPerc(operations, totalErrors)}%"
+                    verbose(Exec + IntegrityString + UniqueString + dataErrString + errRateString)
+            ExecFinStr = f"\n\tExecuted {callback.__name__} {toGenerate:,} times successfully, with total of {operations:,} operations"
+            IntFinString = f"\n\tIntegrityError: {IntE:,} | {calcPerc(operations, IntE)}% of operations"
+            UniqFinString = f"\n\tUniquenessException: {UniqE:,} | {calcPerc(operations, UniqE)}% of operations"
+            DataErrFinString = f"\n\tDataError: {DataE:,} | {calcPerc(operations, DataE)}% of operations"
+            ErrRateFinString = f"\n\tTotal Errors: {totalErrors:,} {calcPerc(operations, totalErrors)}% of operations\n"
+            verbose(ExecFinStr + IntFinString + UniqFinString + DataErrFinString + ErrRateFinString)
 
-    def handle(self, *args, **options):
-        # Connect to an existing database
+    def hardResetdb(self):
         conn = psycopg2.connect("dbname=vbb user=postgres")
 
         # Open a cursor to perform database operations
@@ -137,31 +194,33 @@ class Command(BaseCommand):
         conn.commit()
         cur.close()
         conn.close()
-        # #Insert user-headmaster
-        for i in range(10):
-            self.genUser(600)
-        # Insert user-student
-        for i in range(200):
-            self.genUser(100)
+    
+    def handle(self, *args, **options):
+        self.hardResetdb()
+        # Insert user-headmaster
+        self.exceptionHandlingLoop(self.genUser, NUM_OF_HEADMASTERS_AND_PROGRAMS_AND_SCHOOLS, 600)
+        # # Insert user-student
+        self.exceptionHandlingLoop(self.genUser, NUM_OF_STUDENTS, 100)
         # Insert user-mentor
-        for i in range(50):
-            self.genUser(200)
-        #Insert Program
-        headmaster_id = User.objects.filter(user_type=600)[0].id
-        self.genProgram(headmaster_id)
+        self.exceptionHandlingLoop(self.genUser, NUM_OF_MENTORS, 200)
+        # #Insert Program per headmaster
+        headmasters = [hm.id for hm in User.objects.filter(user_type=600).exclude(deleted=True)] #Headmasters Id's
+        self.exceptionHandlingLoop(self.genProgram, NUM_OF_HEADMASTERS_AND_PROGRAMS_AND_SCHOOLS, headmasters)
+        
         #Insert school
-        program_id = Program.objects.all()[0].id
-        self.genSchool(program_id)
+        programs = [prg.id for prg in Program.objects.all().exclude(deleted=True)]
+        self.exceptionHandlingLoop(self.genSchool, NUM_OF_HEADMASTERS_AND_PROGRAMS_AND_SCHOOLS, programs)
         #Insert Classroom
-        school_id = School.objects.all()[0].id
-        self.genClassroom(school_id)
+        Schools = [ school.id for school in School.objects.all().exclude(deleted=True)] # List of school Id's
+        self.exceptionHandlingLoop(self.genClassroom, NUM_OF_CLASSROOMS, fake.random_elements(Schools, length=NUM_OF_CLASSROOMS))
+
         #Insert Students
-        Croom = Classroom.objects.all()[0] #Classroom Objet
-        students = User.objects.filter(user_type=100)
-        for i in students:
-            userId, classroom_id, school_id = i.id, Croom.id, Croom.school_id
-            self.genStudent(userId, classroom_id, school_id)
-        #Insert Mentors
-        mentors = User.objects.filter(user_type=200)
-        for i in mentors:
-            self.genMentor(i.id)
+        Classrooms = [cls.id for cls in Classroom.objects.all().exclude(deleted=True)] #Classrooms id's
+        students = [std.id for std in User.objects.filter(user_type=100).exclude(deleted=True)]
+        studentAndClassroomPairings = [ (stdId, fake.random_element(Classrooms)) for stdId in students]
+        self.exceptionHandlingLoop(self.genStudent, NUM_OF_STUDENTS, studentAndClassroomPairings)
+        # Insert Mentors
+        mentors = [ mnt.id for mnt in User.objects.filter(user_type=200).exclude(deleted=True)]# Mentor Id's
+        self.exceptionHandlingLoop(self.genMentor, NUM_OF_MENTORS, mentors)
+
+        print('Done')
