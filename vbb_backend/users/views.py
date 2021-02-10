d0d7713608a5d3fec49a12cf249e91e2e6ffc6a7
James Lynott <41302574+jcelynott@users.noreply.github.com>
2021-02-10 18:27:12 -0500

    Resctructured Login method
    Added link to verify Google token info and a filter for the User objects given the provided email

--- vbb_backend/users/views.py
+++ vbb_backend/users/views.py
@@ -1,16 +1,28 @@
 from rest_auth.registration.views import LoginView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import serializers
+from vbb_backend.users.models import User
+import requests
 # from google.oauth2 import id_token
 
 class VBBLogin(LoginView): # accessed from .../api/v1/auth/token, accepts token and returns JWT
+    # TODO:make serializer for request
 
-    adapter_class = GoogleOAuth2Adapter
-    client_class = OAuth2Client
+    def post(self, request):
 
-    def post(request):
-        # TODO: verify token with oauth
-        # user =  id_token.verify_oauth2_token(request.token) # send token field to OAuth to retrieve user
-        return get_refresh_token(user) # send to function to generate token
+        token = request.POST.get("google_access_token")
+
+        auth_url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + str(token)
+        token_info = requests.get(auth_url)
+        
+        email = token_info.GET.get("email")
+        users = User.objects.filter(personal_email = email)
+
+        if len(users) == 1:
+            return get_refresh_token(users[0]) # send to function to generate token
+
+        # TODO: handle errors for invalid emails
+
 
 def get_refresh_token(user):
     refresh = RefreshToken.for_user(user)
