022f051a47e60163fc57facb247bec02a53b379a
James Lynott <41302574+jcelynott@users.noreply.github.com>
2021-02-14 22:46:20 -0500

    Finished writing login view structure
    Still requires additional testing and probably some modifications

--- vbb_backend/users/views.py
+++ vbb_backend/users/views.py
@@ -1,27 +1,27 @@
-from rest_auth.registration.views import LoginView
+from django.views import View
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import serializers
 from vbb_backend.users.models import User
+from django.http import HttpResponse
 import requests
-# from google.oauth2 import id_token
 
-class VBBLogin(LoginView): # accessed from .../api/v1/auth/token, accepts token and returns JWT
-    # TODO:make serializer for request
-
+class VBBLogin(View): # accessed from .../api/v1/auth/token, accepts token and returns JWT
     def post(self, request):
 
-        token = request.POST.get("google_access_token")
+        token = request.POST.get('google_access_token','')
+        if token != '':
+            auth_url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + str(token)
+            token_info = requests.get(auth_url)
+            email = token_info.json['email']
+            users = User.objects.filter(personal_email = email)
 
-        auth_url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + str(token)
-        token_info = requests.get(auth_url)
+            if len(users) == 1:
+                return get_refresh_token(users[0]) # send to function to generate token
+            else:
+                return HttpResponse('Error: no user associated with email') 
+            # TODO: handle case of either no user associated with email or multiple users associated with email
         
-        email = token_info.GET.get("email")
-        users = User.objects.filter(personal_email = email)
-
-        if len(users) == 1:
-            return get_refresh_token(users[0]) # send to function to generate token
-
-        # TODO: handle errors for invalid emails
+        # TODO: handle case where no auth token is posted
 
 
 def get_refresh_token(user):
