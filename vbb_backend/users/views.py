4e41e648ae29c7a4867efe15e0610eb13de19de8
James Lynott <41302574+jcelynott@users.noreply.github.com>
2021-02-16 21:18:03 -0500

    change to jwt formatting

--- vbb_backend/users/views.py
+++ vbb_backend/users/views.py
@@ -32,8 +32,8 @@ def get_refresh_token(user):
     refresh = RefreshToken.for_user(user)
 
     return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token), # lifetime should be specified in settings already
+        'refresh': refresh,
+        'access': refresh.access_token, # lifetime should be specified in settings already
     }
 
 def add_test_user():
