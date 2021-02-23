b975d48ae9f09f6fbf0d1649202b8e0a3d443992
Vignesh Hari <vichuhari100@gmail.com>
2021-02-23 11:25:16 +0530

    Fixed Some Bugs

--- vbb_backend/users/views.py
+++ vbb_backend/users/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.views import View
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import serializers
@@ -6,25 +7,36 @@ from django.http import HttpResponseBadRequest
 from django.http import JsonResponse
 import requests
 
-class VBBLogin(View): # accessed from .../api/v1/auth/token, accepts token and returns JWT
+
+class VBBLogin(View):
+    """
+    accessed from .../api/v1/auth/token, accepts token and returns JWT
+    """
+
     def post(self, request):
-        token = request.POST.get('google_access_token','')
-        if token != '':
-            auth_url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + str(token)
-            token_info = requests.get(auth_url)
-            email = token_info.json().get('email', '')
+        if "google_access_token" in request.POST:
+            try:
+                token = request.POST["google_access_token"]
+                auth_url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + str(
+                    token
+                )
+                token_info = requests.get(auth_url)
+                email = token_info.json().get("email", "")
 
-            if email != '':
-                users = User.objects.filter(personal_email = email).first
+                if email != "":
+                    user = User.objects.filter(email=email).first()
 
-                if users != None:
-                    return JsonResponse(get_refresh_token(users[0])) # send to function to generate token
+                    if user:
+                        return JsonResponse(
+                            get_refresh_token(user)
+                        )  # send to function to generate token
+                    else:
+                        return HttpResponse(status=404)
+                    # TODO: handle case of either no user associated with email or multiple users associated with email
                 else:
-                    return HttpResponseBadRequest('Invalid login')
-                # TODO: handle case of either no user associated with email or multiple users associated with email
-            else:
-                    return HttpResponseBadRequest('Invalid login')
-        
+                    return HttpResponse(status=403)
+            except:
+                return HttpResponse(status=403)
         # TODO: handle case where no auth token is posted
 
 
@@ -32,7 +44,6 @@ def get_refresh_token(user):
     refresh = RefreshToken.for_user(user)
 
     return {
-        'refresh': refresh,
-        'access': refresh.access_token, # lifetime should be specified in settings already
+        "refresh": refresh,
+        "access": refresh.access_token,  # lifetime should be specified in settings already
     }
-        
