99a334ff9f7b77adb36ab7ce69f3d2ebff6647df
danpatrickoneill <danpatrickoneill@gmail.com>
2021-03-02 17:43:45 -0500

    Newsletter sub function working as intended

--- vbb_backend/users/views.py
+++ vbb_backend/users/views.py
@@ -1,27 +1,26 @@
+from pathlib import Path
+import environ
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse
-from rest_framework import status
 
 import mailchimp_marketing as MailchimpMarketing
 from mailchimp_marketing.api_client import ApiClientError
-import json
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+APPS_DIR = ROOT_DIR / "vbb_backend"
+env = environ.Env()
+env.read_env(str(ROOT_DIR / ".env"))
 
 #TODO import and implement Mailchimp API
 @api_view(["POST"])
-def sign_up_for_newsletter(request):  
+def sign_up_for_newsletter(request):
+    # TODO check on desired required fields
     fname = request.data.get("firstName")
     lname = request.data.get("lastName")
     email = request.data.get("email")
     phoneNumber = request.data.get("phoneNumber")
     # countryCode = request.data.get('countryCode'), TODO: when we fix front-end form, we can uncomment this part
-
-    mailchimp = MailchimpMarketing.Client()
-    mailchimp_config = os.path.join("api","mailchimp_config.json")
-    with open(mailchimp_config) as infile:
-        data = json.load(infile)
-    mailchimp.set_config(data['keys'])
-    list_id = data["listid"]["id"]
 
     member_info = {
         "email_address": email,
@@ -35,12 +34,19 @@ def sign_up_for_newsletter(request):
     }
 
     try:
-        response = mailchimp.lists.add_list_member(list_id, member_info)
-        print("response: {}".format(response))
+        client = MailchimpMarketing.Client()
+        client.set_config({
+            "api_key": env.str("MAILCHIMP_API_KEY"),
+            "server": env.str("MAILCHIMP_SERVER")
+        })
+        list_id = env.str("MAILCHIMP_LIST_ID")
+        response = client.lists.add_list_member(list_id, member_info)
+        print(f"Response: {response}")
     except ApiClientError as error:
-        print("An exception occurred: {}".format(error.text))
+        print(f"An exception occurred: {error.text}")
 
     #TODO test this functionality more thoroughly
     return Response(
         {"success": "true"}
     )
+    
