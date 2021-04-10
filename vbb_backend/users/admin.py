9bd37ec2f44ed72cf1ec24bfcd6130b455328f0f
Vignesh Hari <vichuhari100@gmail.com>
2021-04-10 18:38:56 +0530

    Admin Users Editing Completed

--- vbb_backend/users/admin.py
+++ vbb_backend/users/admin.py
@@ -1,19 +1,47 @@
 from django.contrib import admin
-
-
-from vbb_backend.users.models import User, Mentor, Student, HeadMaster
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext
+from django.utils.translation import gettext_lazy as _
+
+from vbb_backend.users.models import HeadMaster, Mentor, Student, User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 
 
-# class MyUserAdmin(UserAdmin):
-#     model = User
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Management",
+            {"fields": ("user_type", "personal_email", "verification_level")},
+        ),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
-#     fieldsets = UserAdmin.fieldsets + (
-#         (None, {"fields": ("user_type", "external_id")}),
-#     )
 
-
-# admin.site.register(User, MyUserAdmin)
+admin.site.register(User, CustomUserAdmin)
 
 admin.site.register(Mentor)
 admin.site.register(Student)
