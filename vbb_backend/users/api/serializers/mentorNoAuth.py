63d076f4122724dd9288b13c217ffdaebfb56ec4
SClausen1 <42357672+SClausen1@users.noreply.github.com>
2021-04-12 14:48:21 -0400

    added password and email fields

--- vbb_backend/users/api/serializers/mentorNoAuth.py
+++ vbb_backend/users/api/serializers/mentorNoAuth.py
@@ -1,19 +1,41 @@
 from django.db.models import fields
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 
-from vbb_backend.users.api.serializers.mentor import MentorUserSerializer
-from vbb_backend.users.models import MentorNoAuth, User, UserTypeEnum
+
+from vbb_backend.users.models import Mentor, User, UserTypeEnum
 
 from rest_framework.exceptions import ValidationError
 from django.db import transaction
 
+class MentorNoAuthUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "date_of_birth",
+            "time_zone",
+            "initials",
+            "personal_email",
+            "phone",
+            "city",
+            "notes",
+            "password",
+            "email"
+        )
+
+    def validate(self, attrs):
+        attrs["user_type"] = UserTypeEnum.MENTOR.value
+        return super().validate(attrs)
+
 
 class MentorNoAuthSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(source="external_id", read_only=True)
-    user = MentorUserSerializer(required=True)
+    user = MentorNoAuthUserSerializer(required=True)
 
     class Meta:
-        model = MentorNoAuth
+        model = Mentor
         exclude = ("deleted", "external_id")
 
     def validate(self, attrs):
@@ -21,12 +43,13 @@ class MentorNoAuthSerializer(serializers.ModelSerializer):
         with transaction.atomic():
             if self.instance:
                 user_obj = self.instance.user
-                user = MentorUserSerializer(user_obj, data=user)
+                user = MentorNoAuthUserSerializer(user_obj, data=user)
                 user.is_valid(raise_exception=True)
                 instance = user.save()
                 attrs["user"] = instance
             else:
-                user = MentorUserSerializer(data=user)
+                user["password"] = make_password(attrs.get("password"))   
+                user = MentorNoAuthUserSerializer(data=user)
                 user.is_valid(raise_exception=True)
                 instance = user.save()
                 attrs["user"] = instance
