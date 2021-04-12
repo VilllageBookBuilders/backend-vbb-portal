79116a16f3fe83c2d59e0aa6038d6aefe8b61a70
SClausen1 <42357672+SClausen1@users.noreply.github.com>
2021-04-12 07:46:05 -0400

    removed duplicate code

--- vbb_backend/users/api/serializers/mentorNoAuth.py
+++ vbb_backend/users/api/serializers/mentorNoAuth.py
@@ -1,35 +1,16 @@
 from django.db.models import fields
 from rest_framework import serializers
 
+from vbb_backend.users.api.serializers.mentor import MentorUserSerializer
 from vbb_backend.users.models import MentorNoAuth, User, UserTypeEnum
 
 from rest_framework.exceptions import ValidationError
 from django.db import transaction
 
 
-class MentorNoAuthUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            "first_name",
-            "last_name",
-            "date_of_birth",
-            "time_zone",
-            "initials",
-            "personal_email",
-            "phone",
-            "city",
-            "notes",
-        )
-
-    def validate(self, attrs):
-        attrs["user_type"] = UserTypeEnum.MENTOR.value
-        return super().validate(attrs)
-
-
 class MentorNoAuthSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(source="external_id", read_only=True)
-    user = MentorNoAuthUserSerializer(required=True)
+    user = MentorUserSerializer(required=True)
 
     class Meta:
         model = MentorNoAuth
@@ -40,12 +21,12 @@ class MentorNoAuthSerializer(serializers.ModelSerializer):
         with transaction.atomic():
             if self.instance:
                 user_obj = self.instance.user
-                user = MentorNoAuthUserSerializer(user_obj, data=user)
+                user = MentorUserSerializer(user_obj, data=user)
                 user.is_valid(raise_exception=True)
                 instance = user.save()
                 attrs["user"] = instance
             else:
-                user = MentorNoAuthUserSerializer(data=user)
+                user = MentorUserSerializer(data=user)
                 user.is_valid(raise_exception=True)
                 instance = user.save()
                 attrs["user"] = instance
